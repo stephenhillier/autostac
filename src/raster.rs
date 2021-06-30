a78d0e4474da0ee8bcbd87e090b34d39fb20d57e
Steve Hillier <stephenhillier@gmail.com>
2021-06-30 08:00:45 -0700

    add comments

--- src/raster.rs
+++ src/raster.rs
@@ -16,17 +16,27 @@ use serde::{Serialize};
 
 use crate::transform;
 
+/// Service represents the raster service, with repositories for spectral imagery and raster data.
+/// The categories and category names are subject to change.
+/// For v0.0.1, the idea is that imagery of various sources can be filtered, or automatically
+/// chosen based on resolution, least cloud coverage, or date ranges. Rasters represent
+/// more varied thematic data like digital elevation models, or derived products (hillshade).
+/// These files can be catalogued by the TIFFTAG_IMAGEDESCRIPTION GeoTIFF tag, and users can
+/// make queries like "what resolution of DEM coverage is here".
 #[derive(Debug)]
 pub struct Service {
     pub imagery: ImageryRepository,
     pub rasters: RasterRepository
 }
 
-
+/// Convert a list of imagery metadata into a GeoJSON FeatureCollection
 pub trait AsFeatureCollection {
   fn as_feature_collection(self) -> FeatureCollection;
 }
 
+/// RasterRepository stores metadata about raster files.
+/// in the future, other storage (besides an in-memory vec) may
+/// be supported.
 #[derive(Debug)]
 pub struct RasterRepository {
   files: Vec<RasterFile>
@@ -40,6 +50,9 @@ impl RasterRepository {
     }
   }
 
+  /// Collect raster files in the /data/raster/ dir and return a vec of
+  /// RasterFile metadata.
+  /// This path should be a CLI argument before v0.0.1.
   pub fn collect_files() -> Vec<RasterFile> {
     let raster_dir = fs::read_dir("./data/raster/").unwrap();
     let mut coverage = Vec::new();
@@ -48,32 +61,31 @@ impl RasterRepository {
       let filename = file.unwrap().path();
       println!("{}", filename.display());
   
-      // open the dataset using GDAL.
-      // panics if cannot be opened by GDAL.  TODO: fix before v0.0.1!
+      // Open the dataset using GDAL.
+      // Panics if cannot be opened by GDAL.  TODO: fix before v0.0.1!
       let dataset = Dataset::open(&filename).unwrap();
       let poly = get_extent(&dataset);
       let projection = dataset.projection();
       let num_bands = dataset.raster_count() as u16;
       
-      // capture the IMAGEDESCRIPTION tag. We can allow users to
+      // Capture the IMAGEDESCRIPTION tag. We can allow users to
       // set this tag as a basic way to group images. e.g. "DEM",
       // "Stream_Burned_DEM". The tag value is up to the user.
       let description: Option<String> = dataset
           .metadata_item("TIFFTAG_IMAGEDESCRIPTION", "");
   
-      // convert extent polygon into EPGS:3857 web mercator
+      // Convert extent polygon into EPGS:3857 web mercator
       // web mercator is used for convenient use with web maps (showing the extents
       // on a map), but this could change (lat/long?)
       let transform_4326_3857 = Proj::new_known_crs(&projection, "EPSG:3857", None).unwrap();
       let boundary: Polygon<f64> = transform::transform_polygon(transform_4326_3857, &poly);
-      // add the file information to the coverage vector.
+      // Add the file information to the coverage vector.
       let properties = RasterFileProperties {
           filename: filename.as_path().display().to_string(),
           resolution: get_resolution_from_geotransform(&dataset.geo_transform().unwrap()),
           description,
           num_bands,
       };
-      println!("{:?}", boundary);
       let file = RasterFile{
           filename,
           boundary,
@@ -84,11 +96,13 @@ impl RasterRepository {
     coverage
   }
 
-  /// returns all the files in RasterRepository.
+  /// Returns all the files in RasterRepository.
   pub fn all(&self) -> &Vec<RasterFile> {
     &self.files
   }
 
+  /// Return files in RasterRepository that intersects with bounds.
+  /// currently, bounds must be web mercator (EPSG:3857).
   pub fn intersects(&self, bounds: &Polygon<f64>) -> Vec<RasterFile> {
     let mut matching_files: Vec<RasterFile> = Vec::new();
     for f in self.files.iter() {
@@ -100,12 +114,16 @@ impl RasterRepository {
   }
 }
 
+/// ImageryRepository stores metadata about spectral imagery files such as
+/// satellite imagery.
 #[derive(Debug)]
 pub struct ImageryRepository {
   files: Vec<ImageryFile>
 }
 
 impl ImageryRepository {
+  /// Create a new ImageryRepository, populated with files found by
+  /// collect_files.
   pub fn new() -> ImageryRepository {
     let files = ImageryRepository::collect_files();
     ImageryRepository{
@@ -185,6 +203,7 @@ impl ImageryRepository {
     &self.files
   }
 
+  /// Returns files in ImageryRepository that intersect with bounds (EPSG:3857)
   pub fn intersects(&self, bounds: &Polygon<f64>) -> Vec<ImageryFile> {
     let mut matching_files: Vec<ImageryFile> = Vec::new();
     for f in self.files.iter() {
@@ -197,6 +216,7 @@ impl ImageryRepository {
 }
 
 impl AsFeatureCollection for &Vec<RasterFile> {
+  /// converts a vec of RasterFiles into a FeatureCollection
   fn as_feature_collection(self) -> FeatureCollection {
     let mut fc = FeatureCollection {
       bbox: None,
@@ -222,6 +242,7 @@ impl AsFeatureCollection for &Vec<RasterFile> {
 }
 
 impl AsFeatureCollection for &Vec<ImageryFile> {
+  /// converts a vec of ImageryFiles into a FeatureCollection
   fn as_feature_collection(self) -> FeatureCollection {
     let mut fc = FeatureCollection {
       bbox: None,
@@ -287,6 +308,7 @@ impl ImageryFileProperties {
   }
 }
 
+/// metadata about spectral images such as satellite imagery
 #[derive(Debug, Clone)]
 pub struct ImageryFile {
   filename: PathBuf,
@@ -313,6 +335,8 @@ impl RasterFileProperties {
     }
 }
 
+/// metadata about a raster data image.
+/// this is "thematic" data such as a DEM or a DEM-derived product.
 #[derive(Debug, Clone)]
 pub struct RasterFile {
     pub filename: PathBuf,
